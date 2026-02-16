e11eefc3a09c02a0922822ecbed306dd11def7d5
Your Name <you@example.com>
2026-02-16 09:37:49 +0100

    app2

--- cw1/src/app/app.ts
+++ cw1/src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('aplikacja 1');
+  info = "To jest zmienna klasy App111";
+  currentDate = new Date();
+  number1 = 5;
+  number2 = 10;
+  showInfo(): string {
+    return this.info + " - metoda showInfo()";
+  }
 }
