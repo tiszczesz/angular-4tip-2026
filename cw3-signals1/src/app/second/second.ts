3d65667f38c199ea545c039643db9147e40906fb
Your Name <you@example.com>
2026-02-26 11:08:26 +0100

    kk

--- cw3-signals1/src/app/second/second.ts
+++ cw3-signals1/src/app/second/second.ts
@@ -8,10 +8,19 @@ import { Component, signal } from '@angular/core';
 })
 export class Second {
   handleInput($event: Event) {
-     //modyfikujemy wartość sygnału, przypisując mu nową wartość z inputa
-     this.message.set(($event.target as HTMLInputElement).value);
+    //modyfikujemy wartość sygnału, przypisując mu nową wartość z inputa
+    this.message.set(($event.target as HTMLInputElement).value);
   }
   //definujemy sygnał, który będzie przechowywał wartość z inputa
-  message = signal('to jest wartość początkowa');
+  message = signal('wartość początkowa');
+  count = signal<number>(0);
+
+  increment() {
+    this.count.update(v => v + 1);
+  }
+
+  decrement() {
+    this.count.update(v => v - 1);
+  }
 
 }
