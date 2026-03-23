dba038661f06d5c2b16be301c133e1022b885433
Your Name <you@example.com>
2026-03-23 08:41:19 +0100

    hh

--- cw5-if/src/app/course/course.ts
+++ cw5-if/src/app/course/course.ts
@@ -8,6 +8,13 @@ import { type Course as courseType, courses } from '../../data/courses';
   styleUrl: './course.css',
 })
 export class Course {
+  enroll() {
+    console.log(`Zapisano na kurs ${this.course().title}`);
+    if (this.course().count < this.course().limit) {
+      this.course().count++;
+      console.log(courses);
+    }
+  }
   //sygnal pobrany z app.ts, który przechowuje aktualnie wybrany kurs
   course = input<courseType>(courses[0]);
 }
