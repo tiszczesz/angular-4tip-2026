09e16c7e98dd032e03f343e3efb9abfd23d30c49
Your Name <you@example.com>
2026-04-10 12:30:12 +0200

    kk

--- cw6-routing/src/app/form-users/form-users.ts
+++ cw6-routing/src/app/form-users/form-users.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
-import { FormField, form } from '@angular/forms/signals';
+import { FormField, email, form, required } from '@angular/forms/signals';
 import { type User, getUsers, getRoles, getLatestUserId } from "../models/usersData";
-import { JsonPipe } from '@angular/common';
+//import { JsonPipe } from '@angular/common';
 // Define an interface for the user form data
 export interface UserForm {
   name: string;
@@ -10,11 +10,16 @@ export interface UserForm {
 }
 @Component({
   selector: 'app-form-users',
-  imports: [FormField, JsonPipe],
+  imports: [FormField],
   templateUrl: './form-users.html',
   styleUrl: './form-users.css',
 })
 export class FormUsers {
+  handleDelete(userToDeletedId: number) {
+    // Update the users signal by filtering out the user with the specified ID
+    this.users.update(users => users.filter(user => user.id !== userToDeletedId));
+
+  }
   handleSubmit($event: SubmitEvent) {
     $event.preventDefault(); // Prevent the default form submission behavior
     const result = this.userModel(); // Get the current value of the userModel signal
@@ -28,12 +33,7 @@ export class FormUsers {
     // Update the users signal by adding the new user to the existing list of users
     this.users.update(users => [...users, newUser]);
     console.log('Form submitted with data:', result);
-    // const newUser: User = {
-    //   id: getLatestUserId() + 1, // Generate a new ID based on the latest user ID
-    //   firstname: this.userForm().name,
-    //   email: this.userForm().email,
-    //   role: this.userForm().role,
-    // };
+
   }
   users = signal<User[]>(getUsers());
   roles = signal<string[]>(getRoles());
@@ -42,5 +42,9 @@ export class FormUsers {
     email: '',
     role: this.roles()[0] || '', // Set default role to the first role in the list
   });
-  userForm = form(this.userModel);
+  userForm = form(this.userModel, (schemaPath) => {
+    required(schemaPath.name, { message: "nazwa jest wymagana" });
+    required(schemaPath.email, { message: "email jest wymagany" });
+    email(schemaPath.email, { message: "nieprawidłowy format email" });
+  });
 }
